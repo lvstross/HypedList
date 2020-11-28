cce4eedea9414a9f5ccc4f8c3997b9d0b1fc70e0
Levi Gonzales <lvstross@gmail.com>
2020-11-28 16:29:19 -0700

    Initial Commit

---
+++ HypedList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HypedList
+//
+//  Created by Levi Gonzales on 11/28/20.
+//  Copyright © 2020 Levi Gonzales. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
